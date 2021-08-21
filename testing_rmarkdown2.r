d89ff7a9c3a0c9ecea8964b729a3e73b28cc1e0b
younger-once <agrzasko@yahoo.com>
2021-08-21 17:30:10 -0500

    adding a bunch of test rmd and r files

---
+++ testing_rmarkdown2.r
@@ -0,0 +1,19 @@
+#'---
+#'title: "stuff"
+#'author: "jj"
+#'date: "8/21/2021"
+#'output: github_document
+
+#'---
+
+#+ r setup, include=FALSE
+knitr::opts_chunk$set(echo = TRUE)
+
+
+## R Markdown
+
+#'This is an R Markdown document. Markdown is a simple formatting syntax for #'authoring HTML, PDF, and MS Word documents. For more details on using R Markdown #'see <http://rmarkdown.rstudio.com>.
+
+#+ r cars
+summary(cars)
+
